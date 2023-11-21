e42362bc0c2caec135ac67299e4953bdafd4964a
noisk8 <juansk8jaramillo@gmail.com>
2023-11-21 00:23:14 -0500

    jala  la busqueda pero no muestra el personaje - Comentarios

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
+// Declaramos una variable para la página actual y la inicializamos en 1
 let paginaActual = 1;
+// Declaramos una variable para los personajes por página y la inicializamos en 5
 const personajesPorPagina = 5;
 
+// Definimos una función para obtener los personajes
 function getCharacters(done){
 
+    // Hacemos una solicitud a la API de los Simpsons
     const results = fetch(`https://apisimpsons.fly.dev/api/personajes?limit=${635}&page=${paginaActual}`);
 
+    // Cuando la solicitud se complete, convertimos la respuesta en JSON
     results.then(response => response.json()
+    // Luego, pasamos los datos a la función done
     .then (data => {
         done(data)
     }))
 
 }
 
+// Definimos una función para mostrar los personajes
 function mostrarPersonajes(data){
+    // Imprimimos los datos en la consola
     console.log(data);
 
+    // Para cada personaje en los datos, creamos un fragmento de HTML
     data.docs.forEach(personaje => {
         const article =document.createRange().createContextualFragment(/*html */`
         
+        // Cada fragmento de HTML es un artículo que contiene información sobre el personaje
         <article>
         <div class="image-container">
           <img src="${personaje.Imagen}" alt="${personaje.Nombre}" />
@@ -34,9 +44,11 @@ function mostrarPersonajes(data){
       `);
         
 
+      // Agregamos el fragmento de HTML al elemento main en la página
       const main = document.querySelector("main");
       main.append(article);
     });
 }
 
+// Llamamos a la función getCharacters y le pasamos la función mostrarPersonajes como argumento
 getCharacters(mostrarPersonajes);
